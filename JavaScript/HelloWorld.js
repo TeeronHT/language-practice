3fa239d112a231948322b707763fb882c48fc4d2
Teeron Hajebi Tabrizi <teeron@hajebi.net>
2022-06-20 01:07:25 -0700

    Running through port 8080

--- JavaScript/HelloWorld.js
+++ JavaScript/HelloWorld.js
@@ -1,2 +1,14 @@
 var msg = 'Hello World';
 console.log(msg);
+
+//This runs through port 8080. To run, type in the URL "http://localhost:8080/"
+/*
+var http = require('http');
+var dt = require('./DateTimeModule');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.dateTime());
+  res.end();
+}).listen(8080);
+*/
