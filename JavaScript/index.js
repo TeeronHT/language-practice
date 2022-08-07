58e4ea190998ccb33f95539cf69467dde8891c0d
TeeronHT <teeron@hajebi.net>
2022-08-06 23:47:58 -0700

    Error with view engine

---
+++ JavaScript/index.js
@@ -0,0 +1,22 @@
+// require the express package in this file
+// and store it in a variable for later use
+const express = require('express');
+
+// create a new instance of `express`
+const app = express();
+
+// Configure application
+app.set('views', __dirname + '/src/views');
+app.set('view engine', 'twig');
+
+// give our app some basic rules about what to
+// do with a web request
+// __dirname returns a value of a path to the current directory
+app.get('/', (request, response) => {
+    response.render('index');
+});
+
+// tell our app what port to listen for requests on
+app.listen(3000, () => {
+    console.log('App is listening on localhost:3000...');
+});
