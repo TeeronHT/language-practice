e742816c2d233ea8fe66d647d67216bbfc911ab0
TeeronHT <teeron@hajebi.net>
2022-08-07 00:41:31 -0700

    Hello World in Swift

---
+++ HelloWorld/HelloWorld/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by Teeron Hajebi on 8/7/22.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Swift/HelloWorld/HelloWorld/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  Hello, World in Swift
+//
+//  Created by Teeron Hajebi on 8/7/22.
+//
+import Foundation
+print("Hello, World!")
+
