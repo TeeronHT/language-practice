70e26220f8b3fce9ecb4b29578b2b9722e71cf21
TeeronHT <teeron@hajebi.net>
2022-08-08 23:16:42 -0700

    TODO: Configure MongoDB

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -13,8 +13,65 @@ app.set('view engine', 'twig');
 // do with a web request
 // __dirname returns a value of a path to the current directory
 app.get('/', (request, response) => {
-    response.render('index');
+    const context = buildContext();
+
+    response.render('pages/home', context);
 });
+
+app.get('/new-thread', (request, response) => {
+    const context = buildContext();
+
+    response.render('pages/new-thread', context);
+});
+
+app.get('/threads', (request, response) => {
+    const context = buildContext();
+    
+    response.render('pages/threads', context);
+});
+
+app.get('/threads/:id', (request, response) => {
+    const id = request.params.id;
+    const context = buildContext({ id: id });
+
+    response.render('pages/thread-detail', context);
+});
+
+app.use('/assets', express.static('src'));
+
+const navigation = [
+    {
+        text: 'Home',
+        type: 'link',
+        url: '/'
+    },
+    {
+        text: 'Threads',
+        type: 'link',
+        url: '/threads',
+    },
+    {
+        text: 'New Thread',
+        type: 'button',
+        url: '/new-thread'
+    }
+];
+
+function buildContext(ctx) {
+    // define the default context for rendering
+    let defaultContext = {
+        navigation: navigation
+    };
+
+    if (!ctx) {
+        // Return the defaultContext
+        return defaultContext;
+    }
+
+    // merge the defaultContext and ctx into an empty object
+    // then return it
+    return Object.assign({}, defaultContext, ctx);
+}
 
 // tell our app what port to listen for requests on
 app.listen(3000, () => {
